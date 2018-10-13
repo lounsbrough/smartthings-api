79d5ca90a3970d4eeaf93dcc00453f76e9df1005
David Lounsbrough <david.lounsbrough@gmail.com>
2018-10-13 13:31:49 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+let app = express()
+const port = 7238
+
+app.use(express.json())
+
+app.post('/', async (req, res) => {
+    let smartthingsService = require('./services/smartthings')
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    if (req.body.authCode && req.body.authCode == process.env.HTTPS_AUTHENTICATION_SECRET) {
+        if (req.body.deviceName && req.body.action) {
+            let response
+            switch (req.body.action) {
+                case 'turnLightOff':
+                    response = await smartthingsService.turnLightOff(req.body.deviceName)
+                    break
+                case 'turnLightOn':
+                    response = await smartthingsService.turnLightOn(req.body.deviceName)
+                    break
+            }
+            res.write(response)
+        }
+    }
+    res.end()
+})
+
+app.listen(port, function () {
+    console.log(`app listening on port ${port}`)
+})
---
+++ services/smartthings.js
@@ -0,0 +1,58 @@
+const smartthings = require('smartthings-node')
+let st = new smartthings.SmartThings(process.env.SMARTTHINGS_PERSONAL_ACCESS_TOKEN)
+
+const findDeviceIdsByName = async (deviceName) => {
+    let matchedDevices = []
+
+    await st.devices.listDevices()
+    .then(deviceList => {
+        deviceList.items.forEach(device => {
+            if ((device.label || device.name).toLowerCase() == deviceName.toLowerCase()) {
+                matchedDevices.push(device)
+            }
+        })
+    })
+
+    return matchedDevices
+}
+
+const turnLightOn = async (deviceName) => {
+    const devicesFound = await findDeviceIdsByName(deviceName)
+
+    if (!Array.isArray(devicesFound) || !devicesFound.length) {
+        return `No devices found for {${deviceName}}`
+    }
+    
+    const commands = [{
+        command: 'on',
+        capability: 'switch',
+        component: 'main',
+        arguments: []
+    }]
+
+    await st.devices.executeDeviceCommand(devicesFound[0].deviceId, commands)
+    return `Device ${devicesFound[0].deviceId} turned on`
+}
+
+const turnLightOff = async (deviceName) => {
+    const devicesFound = await findDeviceIdsByName(deviceName)
+
+    if (!Array.isArray(devicesFound) || !devicesFound.length) {
+        return `No devices found for {${deviceName}}`
+    }
+    
+    const commands = [{
+        command: 'off',
+        capability: 'switch',
+        component: 'main',
+        arguments: []
+    }]
+
+    await st.devices.executeDeviceCommand(devicesFound[0].deviceId, commands)
+    return `Device ${devicesFound[0].deviceId} turned off`
+}
+
+module.exports = {
+    turnLightOff,
+    turnLightOn
+}
