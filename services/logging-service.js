aebff537a267f4c309b043c9b926f15ddabd8276
lounsbrough <lounsbrough@users.noreply.github.com>
2021-08-12 21:54:25 -0500

    Remove file logging

--- services/logging-service.js
+++ services/logging-service.js
@@ -1,21 +1,5 @@
 const { createLogger, format, transports } = require('winston')
 const { combine, timestamp, simple } = format
-require('winston-daily-rotate-file')
-const fs = require('fs')
-
-const loggingDirectory = '../logs'
-
-if (!fs.existsSync(loggingDirectory)) {
-    fs.mkdirSync(loggingDirectory)
-}
-
-const rotatingLogFile = new (transports.DailyRotateFile)({
-    filename: 'logs-%DATE%.log',
-    dirname: loggingDirectory,
-    datePattern: 'YYYY-MM-DD',
-    maxSize: '100m',
-    maxFiles: '30d'
-});
 
 const logger = createLogger({
     level: 'info',
@@ -26,8 +10,7 @@ const logger = createLogger({
         simple()
     ),
     transports: [
-        new transports.Console(),
-        rotatingLogFile
+        new transports.Console()
     ]
 });
 
